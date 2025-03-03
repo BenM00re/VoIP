5bc70bd4fafa8903fa6e67442a4931ca3b49d0a2
BenM00re <148794637+BenM00re@users.noreply.github.com>
2025-03-03 22:43:50 +0000

    Update AudioReceiverThread.java
    Added authentication.

--- AudioReceiverThread.java
+++ AudioReceiverThread.java
@@ -1,65 +1,84 @@
-
 import CMPC3M06.AudioPlayer;
+import uk.ac.uea.cmp.voip.DatagramSocket3;
+import uk.ac.uea.cmp.voip.DatagramSocket2;
+import uk.ac.uea.cmp.voip.DatagramSocket4;
 
 import javax.sound.sampled.LineUnavailableException;
 import java.net.DatagramSocket;
 import java.net.*;
 import java.nio.ByteBuffer;
 
-public class AudioReceiverThread implements Runnable{
-
+public class AudioReceiverThread implements Runnable {
     static DatagramSocket receiving_socket;
 
-    public void start(){
+    public void start() {
         Thread thread = new Thread(this);
         thread.start();
     }
 
-    public void run (){
-        int PORT= 55555;
-        AudioPlayer player = null;
+    public void run() {
+        int PORT = 55555;
+        AudioPlayer player;
+
         try {
             player = new AudioPlayer();
         } catch (LineUnavailableException e) {
             throw new RuntimeException(e);
         }
+
         try {
             receiving_socket = new DatagramSocket(PORT);
         } catch (SocketException e) {
             throw new RuntimeException(e);
         }
-        boolean running=true;
-        byte[] key = "ThisIsAComplexKey123".getBytes();
-        int keyIndex = 0;
-        while(running) {
-            try{
-                byte[] block = new byte[512];
-                DatagramPacket packet = new DatagramPacket(block, block.length);
+
+        boolean running = true;
+        byte encryptionKey = 0x5A; // Same XOR key as sender
+        int receivedPacketCount = 0;
+
+        while (running) {
+            try {
+                byte[] packetData = new byte[514]; // 514 bytes (2 for header + 512 for payload)
+                DatagramPacket packet = new DatagramPacket(packetData, packetData.length);
                 receiving_socket.receive(packet);
-                byte[] decryptedBlock = decrypt(block, key);
-                player.playBlock(decryptedBlock);
+                receivedPacketCount++;
+
+                ByteBuffer VoIPpacket = ByteBuffer.wrap(packet.getData());
+                short receivedKey = VoIPpacket.getShort();
+
+                if (receivedKey != 10) {
+                    System.out.println("Rejected packet #" + receivedPacketCount + " with invalid authentication key: " + receivedKey);
+                    continue;
+                }
+
+                byte[] encryptedBlock = new byte[512];
+                VoIPpacket.get(encryptedBlock);
+
+                byte[] decryptedBlock = decrypt(encryptedBlock, encryptionKey);
+                player.playBlock(encryptedBlock);
+
+                System.out.println("Received and played packet #" + receivedPacketCount + " with authentication key: " + receivedKey);
+
             } catch (Exception e) {
                 e.printStackTrace();
-                running=false;
+                running = false;
             }
         }
+
         receiving_socket.close();
         player.close();
     }
-    // Improved XOR decryption
-    private static byte[] decrypt(byte[] data, byte[] key) {
-        return encrypt(data, key); // XOR decryption is the same as encryption
+
+    // XOR decryption (same as encryption)
+    private static byte[] decrypt(byte[] data, byte key) {
+        return encrypt(data, key); // XOR is reversible
     }
-    // XOR encryption method (same as in AudioSender)
-    private static byte[] encrypt(byte[] data, byte[] key) {
+
+    private static byte[] encrypt(byte[] data, byte key) {
         byte[] encrypted = new byte[data.length];
-        int keyIndex = 0;
-
         for (int i = 0; i < data.length; i++) {
-            encrypted[i] = (byte) (data[i] ^ key[keyIndex]);
-            keyIndex = (keyIndex + 1) % key.length; // Cycle through the key
+            encrypted[i] = (byte) (data[i] ^ key);
         }
-
         return encrypted;
     }
 }
