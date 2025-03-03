97eefb3580c101acdb8a0ebc369419b8dfa5447d
BenM00re <148794637+BenM00re@users.noreply.github.com>
2025-03-03 22:45:44 +0000

    Update AudioSenderThread.java
    Added authentication (and hopefully improved encryption)

--- AudioSenderThread.java
+++ AudioSenderThread.java
@@ -4,63 +4,81 @@ import java.net.*;
 import java.nio.ByteBuffer;
 
 import CMPC3M06.AudioRecorder;
+import uk.ac.uea.cmp.voip.DatagramSocket3;
+import uk.ac.uea.cmp.voip.DatagramSocket2;
+import uk.ac.uea.cmp.voip.DatagramSocket4;
 
 import javax.sound.sampled.LineUnavailableException;
 
-public class AudioSenderThread implements Runnable{
-
+public class AudioSenderThread implements Runnable {
     static DatagramSocket sending_socket;
+    private int packetCount = 0; // Counter for packets
 
-    public void start(){
+    public void start() {
         Thread thread = new Thread(this);
         thread.start();
     }
 
-    public void run (){
-        InetAddress clientIP = null;
+    public void run() {
+        InetAddress clientIP;
         try {
             clientIP = InetAddress.getByName("localhost");
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         }
+
         int PORT = 55555;
-        AudioRecorder recorder = null;
+        AudioRecorder recorder;
+
         try {
             recorder = new AudioRecorder();
         } catch (LineUnavailableException e) {
             throw new RuntimeException(e);
         }
+
         try {
             sending_socket = new DatagramSocket();
         } catch (SocketException e) {
             throw new RuntimeException(e);
         }
+
         boolean running = true;
-            byte[] key = "ThisIsAComplexKey123".getBytes();
-            int keyIndex = 0;
-            while(running) {
-                try {
-                    byte[] block = recorder.getBlock();
-                    byte[] encryptedBlock = encrypt(block, key); // Encrypt the block
-                    DatagramPacket packet = new DatagramPacket(encryptedBlock, encryptedBlock.length, clientIP, PORT);
-                    sending_socket.send(packet);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    running = false;
-                }
-            }
-            sending_socket.close();
-            recorder.close();
-        }
-        private static byte[] encrypt(byte[] data, byte[] key) {
-            byte[] encrypted = new byte[data.length];
-            int keyIndex = 0;
+        byte encryptionKey = 0x5A; // Simple fixed XOR key
 
-            for (int i = 0; i < data.length; i++) {
-                encrypted[i] = (byte) (data[i] ^ key[keyIndex]);
-                keyIndex = (keyIndex + 1) % key.length; // Cycle through the key
-            }
+        while (running) {
+            try {
+                byte[] block = recorder.getBlock();
+                byte[] encryptedBlock = encrypt(block, encryptionKey); // Encrypt using simple XOR
 
-            return encrypted;
+                ByteBuffer VoIPpacket = ByteBuffer.allocate(514);
+                //short authenticationKey = (short) ((packetCount % 5 == 0) ? 99 : 10); // Every 5th packet is invalid
+                short authenticationKey = 10; //stable
+                packetCount++;
+
+                VoIPpacket.putShort(authenticationKey);
+                VoIPpacket.put(encryptedBlock);
+
+                DatagramPacket packet = new DatagramPacket(VoIPpacket.array(), 514, clientIP, PORT);
+                sending_socket.send(packet);
+
+                System.out.println("Sent packet #" + packetCount + " with authentication key: " + authenticationKey);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                running = false;
+            }
         }
+
+        sending_socket.close();
+        recorder.close();
+    }
+
+    // Simplified XOR encryption (single-byte key)
+    private static byte[] encrypt(byte[] data, byte key) {
+        byte[] encrypted = new byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            encrypted[i] = (byte) (data[i] ^ key); // XOR each byte with the key
+        }
+        return encrypted;
+    }
 }
