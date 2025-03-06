b0e4888ef531bde930ee3c7aea4d2fb254515b00
BenM00re <148794637+BenM00re@users.noreply.github.com>
2025-03-06 14:37:18 +0000

    Update AudioDuplex.java
    added other sockets

--- AudioDuplex.java
+++ AudioDuplex.java
@@ -1,16 +1,24 @@
 public class AudioDuplex {
 
+    public static void main(String[] args) {
+        try {
+            //RECEIVERS
+            AudioReceiverThread receiver = new AudioReceiverThread();
+            AudioReceiverThread2 receiver2 = new AudioReceiverThread2();
+            AudioReceiverThread3 receiver3 = new AudioReceiverThread3();
+            AudioReceiverThread4 receiver4 = new AudioReceiverThread4();
+            //SENDERS
+            AudioSenderThread sender = new AudioSenderThread();
+            AudioSenderThread2 sender2 = new AudioSenderThread2();
+            AudioSenderThread3 sender3 = new AudioSenderThread3();
+            AudioSenderThread4 sender4 = new AudioSenderThread4();
 
-    public static void main (String[] args){
+            //CHANGE NUMBER BASED ON SOCKET
+            receiver4.start();
+            sender4.start();
 
-        AudioReceiverThread receiver = new AudioReceiverThread();
-        AudioSenderThread sender = new AudioSenderThread();
-
-        receiver.start();
-        sender.start();
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
-
-
